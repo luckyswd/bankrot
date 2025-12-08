4804bc0f60974d1bab7c748ad3be60b0bcb663cb
maksimshediakov <shedyakovm@gmail.com>
2025-12-08 13:49:42 +0300

    5. Сделать что бы плейсхолдер был всегда видимый, что бы было всегда перед глазами. Так же плейсхолдеры сгенерить реальные.

--- frontend/src/components/ClientCard/General/Passport.tsx
+++ frontend/src/components/ClientCard/General/Passport.tsx
@@ -17,47 +17,51 @@ export const PassportInfo: FC<Props> = ({ register, control }) => {
     <AccordionItem value="passportInfo">
       <AccordionTrigger><h3 className="text-xl font-semibold">Паспорт</h3></AccordionTrigger>
       <AccordionContent className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 p-2">
-        <div className="space-y-2">
+        <div className="space-y-1">
           <Label htmlFor="basic_info.passportSeries">Серия паспорта</Label>
           <Input
             id="basic_info.passportSeries"
-            placeholder="4016"
+            placeholder="4512"
             maxLength={10}
             {...register("basic_info.passportSeries")}
           />
+          <p className="text-xs text-muted-foreground/70 italic">Пример: 4512</p>
         </div>
 
-        <div className="space-y-2">
+        <div className="space-y-1">
           <Label htmlFor="basic_info.passportNumber">Номер паспорта</Label>
           <Input
             id="basic_info.passportNumber"
-            placeholder="123456"
+            placeholder="878508"
             maxLength={20}
             {...register("basic_info.passportNumber")}
           />
+          <p className="text-xs text-muted-foreground/70 italic">Пример: 878508</p>
         </div>
 
-        <div className="space-y-2">
+        <div className="space-y-1">
           <Label htmlFor="basic_info.passportDepartmentCode">
             Код подразделения
           </Label>
           <Input
             id="basic_info.passportDepartmentCode"
-            placeholder="780-089"
+            placeholder="780-001"
             maxLength={20}
             {...register("basic_info.passportDepartmentCode")}
           />
+          <p className="text-xs text-muted-foreground/70 italic">Пример: 780-001</p>
         </div>
 
-        <div className="space-y-2 lg:col-span-3">
+        <div className="space-y-1 lg:col-span-3">
           <Label htmlFor="basic_info.passportIssuedBy">
             Кем выдан паспорт
           </Label>
           <Input
             id="basic_info.passportIssuedBy"
-            placeholder="ОУФМС России по СПб и ЛО в Московском районе"
+            placeholder="Отделом УФМС России по Санкт-Петербургу и Ленинградской области в Центральном районе"
             {...register("basic_info.passportIssuedBy")}
           />
+          <p className="text-xs text-muted-foreground/70 italic">Пример: Отделом УФМС России по Санкт-Петербургу и Ленинградской области в Центральном районе</p>
         </div>
 
         <Controller
