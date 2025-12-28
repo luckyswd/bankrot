c44ce7902f63f7dad0ee1577901795606e434332
Denis <d.shedyakov@nag.ru>
2025-12-28 20:15:32 +0300

    add ROLE_FINANCIAL_MANAGER

--- backend/migrations/Version20251228171509.php
+++ backend/migrations/Version20251228171509.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251228163852 extends AbstractMigration
+final class Version20251228171509 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,11 +21,17 @@ final class Version20251228163852 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE financial_managers (created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, id INT AUTO_INCREMENT NOT NULL, fio VARCHAR(255) DEFAULT NULL, inn VARCHAR(12) DEFAULT NULL, snils VARCHAR(14) DEFAULT NULL, arbitration_manager_registry_number VARCHAR(50) DEFAULT NULL, email VARCHAR(255) DEFAULT NULL, phone VARCHAR(20) DEFAULT NULL, aau_name VARCHAR(255) DEFAULT NULL, aau_ogrn VARCHAR(15) DEFAULT NULL, aau_inn VARCHAR(12) DEFAULT NULL, aau_address LONGTEXT DEFAULT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('ALTER TABLE contracts ADD financial_manager_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE contracts ADD CONSTRAINT FK_950A973FD5CC44A FOREIGN KEY (financial_manager_id) REFERENCES financial_managers (id) ON DELETE SET NULL');
+        $this->addSql('CREATE INDEX IDX_950A973FD5CC44A ON contracts (financial_manager_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE financial_managers');
+        $this->addSql('ALTER TABLE contracts DROP FOREIGN KEY FK_950A973FD5CC44A');
+        $this->addSql('DROP INDEX IDX_950A973FD5CC44A ON contracts');
+        $this->addSql('ALTER TABLE contracts DROP financial_manager_id');
     }
 }
