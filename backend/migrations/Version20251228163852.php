6ffccdc3fef42279019109b56c8a6fc82a828808
Denis <d.shedyakov@nag.ru>
2025-12-28 20:11:56 +0300

    add ROLE_FINANCIAL_MANAGER

--- backend/migrations/Version20251228163852.php
+++ backend/migrations/Version20251228163852.php
@@ -21,13 +21,11 @@ final class Version20251228163852 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE financial_managers (created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, id INT AUTO_INCREMENT NOT NULL, fio VARCHAR(255) DEFAULT NULL, inn VARCHAR(12) DEFAULT NULL, snils VARCHAR(14) DEFAULT NULL, arbitration_manager_registry_number VARCHAR(50) DEFAULT NULL, email VARCHAR(255) DEFAULT NULL, phone VARCHAR(20) DEFAULT NULL, aau_name VARCHAR(255) DEFAULT NULL, aau_ogrn VARCHAR(15) DEFAULT NULL, aau_inn VARCHAR(12) DEFAULT NULL, aau_address LONGTEXT DEFAULT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
-        $this->addSql('ALTER TABLE contracts ADD CONSTRAINT FK_950A973783E3463 FOREIGN KEY (manager_id) REFERENCES financial_managers (id) ON DELETE SET NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE financial_managers');
-        $this->addSql('ALTER TABLE contracts DROP FOREIGN KEY FK_950A973783E3463');
     }
 }
